474d25d8fab69135d828c048f051b519bb05776b
Felipe Botero <59348629+FeBotero@users.noreply.github.com>
2023-03-18 17:48:53 -0400

    create type user

--- backend/src/model/user.model.ts
+++ backend/src/model/user.model.ts
@@ -1,17 +1,23 @@
 import mongoose from "mongoose";
 
+export interface IUser {
+  name: String;
+  email: String;
+  password: String;
+}
+
 const userSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        require:true
-    },
-    email:{
-        type:String,
-        require:true
-    },
-    password:{
-        type:String,
-        require:true
-    }
-})
-export const User = mongoose.model("User",userSchema)
+  name: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+});
+export const User = mongoose.model("User", userSchema);
