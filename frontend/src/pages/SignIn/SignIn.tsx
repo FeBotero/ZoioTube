7f34e73eb84abb2f09b1c7bd010dd5abc140a46a
Felipe Botero <59348629+FeBotero@users.noreply.github.com>
2023-03-19 10:55:08 -0400

    Correção no login google

--- frontend/src/pages/SignIn/SignIn.tsx
+++ frontend/src/pages/SignIn/SignIn.tsx
@@ -1,12 +1,15 @@
 import logoGoogle from "../../assets/LogoGoogle.png"
 import * as S from "./styles"
-
+import { useAuth } from "../../hooks/useAuth"
 
 export function SignIn(){
+    const {user,singInWhithGoogle} = useAuth()
+
+    console.log(user)
     return(
         <S.containerSingIn>
   
-        <button >
+        <button onClick={singInWhithGoogle}>
            <img src={logoGoogle} alt="Google" /> Login google
         </button>
         
