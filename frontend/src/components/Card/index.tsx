e34947d9268ab7cab690bd9532ee6fb109fe5167
Felipe Botero <59348629+FeBotero@users.noreply.github.com>
2023-03-20 23:04:37 -0400

    stylized card component

--- frontend/src/components/Card/index.tsx
+++ frontend/src/components/Card/index.tsx
@@ -1,6 +1,21 @@
+import teste from "../../assets/JogoTeste.jpg"
+import author from "../../assets/author.jpg"
+import { ContainerCard } from "./styles"
+
 export function Card(){
     return(
-        <>
-        </>
+        <ContainerCard>
+            <img className="videoPreview" src={teste} alt="" />
+            <div className="contentAuthor">
+
+            <img className="imgAuthor" src={author} alt="Autor" />
+            <div >
+
+            <h3>Title</h3>
+            <p>Felipe Botero</p>
+            </div>
+            </div>
+
+        </ContainerCard>
     )
 }
---
+++ frontend/src/components/Card/styles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const ContainerCard = styled.div`
+  cursor: pointer;
+  .videoPreview {
+    width: 20rem;
+  }
+  .imgAuthor {
+    width: 3rem;
+    border-radius: 50%;
+  }
+  .contentAuthor {
+    display: flex;
+    gap: 1rem;
+    div {
+    }
+  }
+`;
