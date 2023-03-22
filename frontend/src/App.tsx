8c54bcd9be394dd647aa02be44e66ee6b8dae7ce
Felipe Botero <59348629+FeBotero@users.noreply.github.com>
2023-03-22 14:16:17 -0400

    update styles App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,18 @@
-
-import { Header } from './components/Header/Header'
-import { Footer } from './components/Footer/Footer'
-import { Outlet } from 'react-router-dom'
-import "./styles/global.css"
-
+import { Header } from "./components/Header/Header";
+import { Footer } from "./components/Footer/Footer";
+import { Outlet } from "react-router-dom";
+import "./styles/global.css";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Outlet/>
-      <Footer/>
-        
+      <Header />
+      <div className="content">
+        <Outlet />
+      </div>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
